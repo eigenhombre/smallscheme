4a55c0f30628997df973b3ca0434cba2592d4872
John Jacobsen <eigenhombre@gmail.com>
2022-07-17 21:26:24 -0500

    Placate PEP8. #2

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ import os
 from setuptools import setup
 
 setup(
-    name = "smallscheme",
-    version = "0.0.1",
-    author = "John Jacobsen",
-    author_email = "eigenhombre@gmail.com",
-    description = ("A small implementation of enough Scheme "
+    name="smallscheme",
+    version="0.0.1",
+    author="John Jacobsen",
+    author_email="eigenhombre@gmail.com",
+    description=("A small implementation of enough Scheme "
                    "to follow along in SICP"),
-    license = "MIT",
-    keywords = "lisp sicp scheme",
-    url = "https://github.com/eigenhombre/smallscheme",
-    packages = ['smallscheme'],
-    classifiers = [
+    license="MIT",
+    keywords="lisp sicp scheme",
+    url="https://github.com/eigenhombre/smallscheme",
+    packages=['smallscheme'],
+    classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Languages",
         "License :: OSI Approved :: MIT License",
